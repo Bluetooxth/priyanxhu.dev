efe4893bc39bc4471f359bc6019bf63206622ec5
Priyanshu Chahar <priyannxhuu@gmail.com>
2024-12-23 21:12:56 +0530

    fixed typo

--- components/Home/Projects.jsx
+++ components/Home/Projects.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import projexts from "@/data/Projects";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
-import { IoGlobeOutline } from "react-icons/io5";
+import { LuPlugZap } from "react-icons/lu";
 
 const Projects = () => {
   return (
     <section className="flex justify-center items-center w-full">
       <div className="flex flex-col justify-start items-start w-[95vw] md:w-[80vw] lg:w-[70vw] xl:w-[60vw] 2xl:w-[50vw] px-2 md:px-0 gap-4">
         <h2 className="text-2xl md:text-3xl font-medium text-zinc-100">
-          Thing i worked on
+          Things i worked on
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 justify-start items-stretch gap-3 w-full">
           {projexts.map((project, index) => (
@@ -19,10 +19,18 @@ const Projects = () => {
                 "
               key={index}
             >
-              <div className="flex flex-col justify-start items-start gap-1">
-                <h3 className="text-xl font-medium text-zinc-200">
-                  {project.name}
-                </h3>
+              <div className="flex flex-col justify-start items-start gap-1 w-full">
+                <div className="flex justify-between items-center w-full">
+                  <Link
+                    href={project.link}
+                    className="text-xl font-medium text-zinc-200 flex items-center gap-2 underline"
+                  >
+                    <LuPlugZap className="" /> {project.name}
+                  </Link>
+                  <Link href={project.link} className="text-zinc-200">
+                    <FaGithub className="text-2xl text-zinc-300" />
+                  </Link>
+                </div>
                 <p className="text-lg text-zinc-300">{project.desc}</p>
               </div>
               <div className="flex flex-col justify-start items-start gap-2 h-full mt-2">
@@ -32,28 +40,6 @@ const Projects = () => {
                       {icon}
                     </span>
                   ))}
-                </div>
-                <div className="flex flex-wrap justify-start items-center gap-2 mt-2">
-                  <Link
-                    target="_blank"
-                    href={project.github}
-                    className="
-                    text-md font-medium text-zinc-200 bg-zinc-900 hover:bg-zinc-900 border-2 border-zinc-300 py-1 px-3 rounded-lg transition-all duration-300 ease-in-out flex items-center gap-2
-                    "
-                  >
-                    Github <FaGithub className="inline-block" />
-                  </Link>
-                  {project.link && (
-                    <Link
-                      target="_blank"
-                      href={project.link}
-                      className="
-                    text-md font-medium text-zinc-200 bg-zinc-900 hover:bg-zinc-900 border-2 border-zinc-300 py-1 px-3 rounded-lg transition-all duration-300 ease-in-out flex items-center gap-2
-                    "
-                    >
-                      Live <IoGlobeOutline className="inline-block" />
-                    </Link>
-                  )}
                 </div>
               </div>
             </div>
